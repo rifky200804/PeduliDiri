4c670b24a35db48660df26a8b9965212b4878570
unknown <zakyabillah344@gmail.com>
2022-04-13 11:57:42 +0700

    dashboard_fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,27 @@
             <div class="col-md-6 mt-9">
                 <h1>Selamat Datang {{ Auth::user()->nama }} Di Peduli Diri</h1>
                 <h3 class="mb-7">Anda Dapat Menggunakan PEDULI DIRI Untuk Mencatat Data Perjalanan Anda</h3>
-
+                <div class="row">
+                    <div class="col-md-3 text-center">
+                        <a href="{{route('perjalanan.data')}}" title="data Perjalanan">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
+                                <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                            </svg>
+                            <br> Data Perjalanan
+                        </a>
+                    </div>
+                    @if(Auth::user()->role == 'admin')
+                    <div class="col-md-3 text-center" >
+                        <a href="{{route('user.data')}}" title="data user">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
+                                <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                            </svg>
+                            <br> Data User
+                        </a>
+                    </div>
+                    @endif
+                </div>
+                
             </div>
             <div class="col-md-6">
                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
